b58e64fa0f26bafb883c010da15a5bc9bac446ca
Pavel Kozheykin <pavel.kozheykin@basetis.com>
2022-04-13 18:29:46 +0200

    Feature :: Register action test and request reset password

--- api/tests/Functional/User/RegisterActionTest.php
+++ api/tests/Functional/User/RegisterActionTest.php
@@ -24,4 +24,33 @@ class RegisterActionTest extends UserTestBase
         $this->assertEquals(JsonResponse::HTTP_CREATED, $response->getStatusCode());
         $this->assertEquals($payload['email'], $responseData['email']);
     }
+
+    public function testRegisterWithMissingParameters(): void
+    {
+        $payload = [
+            'name' => 'Stewie',
+            'password' => '123456'
+        ];
+
+        self::$client->request('POST', \sprintf('%s/register', $this->endpoint), [], [], [], \json_encode($payload));
+
+        $response = self::$client->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_BAD_REQUEST, $response->getStatusCode());
+    }
+
+    public function testRegisterWithInvalidPassword(): void
+    {
+        $payload = [
+            'name' => 'Stewie',
+            'email' => 'stewie@api.com',
+            'password' => '1'
+        ];
+
+        self::$client->request('POST', \sprintf('%s/register', $this->endpoint), [], [], [], \json_encode($payload));
+
+        $response = self::$client->getResponse();
+
+        $this->assertEquals(JsonResponse::HTTP_BAD_REQUEST, $response->getStatusCode());
+    }
 }
